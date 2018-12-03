78d1b9a5de3160f275f909ee178dd62a33c9af25
caitlinyang <hlic_yang@yahoo.com>
2018-12-03 02:34:44 -0500

    adding logout functionality and new dashboard page

--- forms.py
+++ forms.py
@@ -15,8 +15,8 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
    
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2,max=20)])
     password = PasswordField('Password',
                              validators=[DataRequired()])
     submit = SubmitField('Login') 
