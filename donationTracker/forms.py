e19317ac789eaf2310377efc06225ae078cd19ce
caitlinyang <hlic_yang@yahoo.com>
2018-12-03 23:12:34 -0500

    M5 adding persistance and user login

--- donationTracker/forms.py
+++ donationTracker/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from donationTracker.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -13,10 +14,22 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     user_type = SelectField('User Type', choices=[('user','User'),('location_employee','Location Employee'),('admin','Admin')])
     submit = SubmitField('Sign Up')
-   
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose another username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please pick another email.')
+
+
 class LoginForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2,max=20)])
     password = PasswordField('Password',
                              validators=[DataRequired()])
-    submit = SubmitField('Login') 
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
